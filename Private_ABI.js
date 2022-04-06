3202077ff4d4b27383463cfd0fc4090c474f9476
Jawsh <jradingrant@gmail.com>
2022-04-06 13:28:16 +0100

    private

--- Private_ABI.js
+++
@@ -1,3 +0,0 @@
-export const RINKEBY_API = "https://rinkeby.infura.io/v3/31f62705b513402f90371112bc818aa9";
-
-export const RINKEBY_PRIVATE_KEY = "633f76c1ebb6b495f2c41fcf08f753235ed5d05797d0a20251d3e9d9dca13d43";
